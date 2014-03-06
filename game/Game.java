c98ad8ffa2e1ec4b2a24fd09c2c6e5ebb6b6eedb
Nicolas KC <nicolas@kolnescassis.com>
2014-03-07 00:20:21 +0100

    Added regions to squares and added region to boardGenerator.

--- game/Game.java
+++ game/Game.java
@@ -15,24 +15,100 @@ public class Game {
 		System.out.print(n);
 	}
 
+	/**
+	 * 
+	 * @param size
+	 * @return List of board
+	 * 
+	 * This method creates the board. It randomises each and every square
+	 * and then it adds the regions. Regions are always FOREST at start
+	 * and LAVA in the end, with random regions in the middle.
+	 */
 	public static List<DefaultSquare> generateBoard(int size) {
-
+		/*
+		 * Init and declare all required lists to generate positions,
+		 * regions and later sorting positions in correct order.
+		 */
 		List<DefaultSquare> board = new ArrayList<DefaultSquare>();
 		List<Integer> position = new ArrayList<Integer>();
+		List<Region> regions = new ArrayList<Region>();
+		List<Region> regionOrder = regionList(5);
 		
-		for (int i = 1; i < size; i++)
-			position.add(i);
+		/*
+		 * This is where the randomised regionOrder is used to create a list as 
+		 * large as the board itself with all the regions. In the same loop,
+		 * positions are being saved in a positions array.
+		 */
+		for (Region r: regionOrder)
+			for (int a = 0; a < size/regionOrder.size(); a++){
+				regions.add(r);
+				position.add(position.size());
+			}
 		
+		// to make room for the home square, position 0 is removed.
+		position.remove(0);
+		
+		// this will now make a list of 99 unique numbers in a random order
 		Collections.shuffle(position);
 		
+		/*
+		 * As if the random position order wasn't enough, the order the squares
+		 * are added is random too. Now random squares are added up with
+		 * random positions. Square 0 is reserved for the home square.
+		 */
 		board.add(new HomeSquare(0)); 
 		for(int p : position) {
-			board.add((Math.random()<0.2) ? new ResetSquare(p) : new MobSquare(p));
+			board.add((Math.random()<0.1) ? 
+					new ResetSquare(p) : new MobSquare(p));
+			//board.add(new HomeSquare(p));
 		}
 		
+		// Now the entire board is sorted by square positions.
 		Collections.sort(board);
 		
+		//write("Board size: " + board.size());
+		//write(" Regions size: " + regions.size() + "\n\n");
+		
+		/*
+		 * This is where the regions are added to the list. The entire board
+		 * runs though the regions list, adding one region at a time.
+		 */
+		for(DefaultSquare d: board){
+			d.setRegion(regions.get(0));
+			regions.remove(0);
+		}
+			
+		
 		return board;
+	}
+	
+	/**
+	 * 
+	 * @param amountRegions
+	 * @return random sorted region list
+	 * 
+	 * The regionList generates a list with as many regions as the method int 
+	 * is set to. The method mixes up all available regions from the Region
+	 * enum and cuts down the list to match the size.
+	 * The unique thing with this method is that it ensures the first Region is
+	 * FOREST and the last Region is LAVA, with the middle Regions being mixed.
+	 */
+	public static List<Region> regionList(int amountRegions) {
+		List<Region> regionOrder = new ArrayList<Region>();
+		// Get all regions except FOREST and LAVA
+		for (Region r: Region.values())
+			if(r != Region.FOREST && r != Region.LAVA)
+				regionOrder.add(r);
+		
+		Collections.shuffle(regionOrder);
+		
+		while(regionOrder.size() > amountRegions-2)
+			regionOrder.remove(0);
+		// Add start region
+		regionOrder.add(0, Region.FOREST);
+		// Add end region
+		regionOrder.add(Region.LAVA);
+		return regionOrder;
 	}
 
 	public static List<Player> fetchPlayers(Scanner in) {
@@ -58,8 +134,8 @@ public class Game {
 		Scanner in = new Scanner(System.in);
 		
 		List<Player> players = fetchPlayers(in);	// generate players
-		List<DefaultSquare> board = generateBoard(players.get(0).getDice()
-				.getRolls()*20);	// generate board
+		// OBS! Board size must be at least 60 and divisible by 20
+		List<DefaultSquare> board = generateBoard(100);	// generate board
 		
 		int move, dice, turn;
 		move = dice = turn = 0;
